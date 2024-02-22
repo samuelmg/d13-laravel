75efb0c14c300b189a2932c3d554a39e1dba9d32
Samuel Mercado <samuel.mg@gmx.com>
2024-02-22 16:21:02 -0600

    Instalación de Jetsream con Livewire

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,12 @@ Route::post('/contacto', function(Request $request) {
     // return redirect('/contacto');
     return redirect()->back();
 });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
